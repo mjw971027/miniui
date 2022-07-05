70836f2119923fae5f1f303a522b4f49b3c716a4
mjw971027 <916753927@qq.com>
2022-07-05 16:32:52 +0800

    Create README.md

--- src/test/java/com/example/miniui/huiwen.java
+++ src/test/java/com/example/miniui/huiwen.java
@@ -1,12 +1,22 @@
 package com.example.miniui;
 
+
 import java.util.*;
 
+import org.apache.commons.lang3.StringUtils;
 public class huiwen {
     public static void main(String[] args) {
         int[] num = {3, 2, 3, 2, 2, 2};
-        int[] res = nextGreaterElements(num);
-        boolean res2 = divideArray(num);
+//        int[] res = nextGreaterElements(num);
+//        boolean res2 = divideArray(num);
+        List<String> dictionary = new ArrayList<>();
+        dictionary.add("cat");
+        dictionary.add("rat");
+        dictionary.add("bat");
+        String sentence = "the cattle was rattled by the battery";
+        String res = replaceWords(dictionary, sentence);
+
+
         return;
     }
 
@@ -116,5 +126,25 @@ public class huiwen {
         }
     }
 
+    public static String replaceWords(List<String> dictionary, String sentence) {
+        String[] res = sentence.split(" ");
+        String[] end2 = new String[res.length];
+        List<String> list = new ArrayList<String>();
+        for (int i = 0; i < res.length; i++) {
+            for (int j = 0; j < dictionary.size(); j++) {
+                System.out.println(res[i].indexOf(dictionary.get(j).toString()));
+                if (res[i].indexOf(dictionary.get(j).toString()) == 0) {
+                    end2[i] = dictionary.get(j).toString();
+                    break;
+                } else if (res[i].indexOf(dictionary.get(j).toString()) == -1) {
+                    if (j == dictionary.size() - 1) {
+                        end2[i] = res[i];
+                    }
+                }
+            }
+        }
+        String end = Arrays.toString(end2);
+        return end;
+    }
 
 }
