cb3b7f865559589f0807595f3a6f014f7d1efbdc
mjw971027 <916753927@qq.com>
2022-07-13 16:52:20 +0800

    Create README.md

--- src/test/java/com/example/miniui/huiwen.java
+++ src/test/java/com/example/miniui/huiwen.java
@@ -298,7 +298,6 @@ public class huiwen {
             System.out.println(tmp);
         }
     }
-
     class Solution {
         public List<List<Integer>> permute(int[] nums) {
             List<List<Integer>> res = new ArrayList<List<Integer>>();
@@ -387,7 +386,6 @@ public class huiwen {
 
         }
     }
-
     private void dfs3(int[] nums, int len, int depth,
                       Deque<Integer> path, boolean[] used,
                       List<List<Integer>> res) {
@@ -406,5 +404,74 @@ public class huiwen {
         }
     }
 
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        int len = candidates.length;
+        List<List<Integer>> res = new ArrayList<>();
+        if (len == 0) {
+            return res;
+        }
+        List<Integer> path = new ArrayList<>();
+        dfs4(candidates, len, 0, target, path, res);
+        return res;
+
+    }
+
+    //    要注意新的分支时要去掉之前未选择的
+    public void dfs4(int[] nums, int len, int begin, int target, List<Integer> path, List<List<Integer>> res) {
+        if (target < 0) {
+            return;
+        }
+        if (target == 0) {
+            res.add(path);
+            return;
+        }
+        for (int i = begin; i < len; i++) {
+            path.add(nums[i]);
+            dfs4(nums, len, begin, target - nums[i], path, res);
+            path.remove(path.size() - 1);
+        }
+    }
+
+    public List<List<Integer>> combinationSum1(int[] candidates, int target) {
+        int len = candidates.length;
+        List<List<Integer>> res = new ArrayList<>();
+        if (len == 0) {
+            return res;
+        }
+
+        Deque<Integer> path = new ArrayDeque<>();
+        dfs11(candidates, 0, len, target, path, res);
+        return res;
+    }
+
+    /**
+     * @param candidates 候选数组
+     * @param begin      搜索起点
+     * @param len        冗余变量，是 candidates 里的属性，可以不传
+     * @param target     每减去一个元素，目标值变小
+     * @param path       从根结点到叶子结点的路径，是一个栈
+     * @param res        结果集列表
+     */
+    private void dfs11(int[] candidates, int begin, int len, int target, Deque<Integer> path, List<List<Integer>> res) {
+        // target 为负数和 0 的时候不再产生新的孩子结点
+        if (target < 0) {
+            return;
+        }
+        if (target == 0) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        // 重点理解这里从 begin 开始搜索的语意
+        for (int i = begin; i < len; i++) {
+            path.addLast(candidates[i]);
+
+            // 注意：由于每一个元素可以重复使用，下一轮搜索的起点依然是 i，这里非常容易弄错
+            dfs11(candidates, i, len, target - candidates[i], path, res);
+
+            // 状态重置
+            path.removeLast();
+        }
+    }
 
 }
